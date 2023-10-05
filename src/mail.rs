dd0947ba8600e9816bd2dfb454b94a1587b64390
Justin <justin@garofolo.net>
2023-10-04 22:20:28 -0400

    periodically scrape mail, refactos

--- src/mail.rs
+++ src/mail.rs
@@ -5,11 +5,6 @@ use serde::Deserialize;
 const DISPOSABLE_MAIL: &str = "https://www.disposablemail.com/";
 const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0";
 
-pub struct Inbox {
-    client: Client,
-    pub address: String,
-}
-
 impl Inbox {
     pub fn new() -> Self {
         let client = Client::builder()
@@ -19,34 +14,85 @@ impl Inbox {
 
         Inbox {
             client,
-            address: "".to_string(),
+            address_info: None,
         }
     }
 
     pub async fn establish_address(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         self.client.get(DISPOSABLE_MAIL)
+            .headers(headers(false))
             .send()
             .await?;
 
         let response = self.client.get(format!("{}index/index", DISPOSABLE_MAIL))
             .headers(headers(true))
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
 
-        if response.status().is_success() {
-            let text = response.text().await?;
-            let data: AddressInfo = serde_json::from_str(&text)?;
-            //println!("{:?}", data);
-            self.address = data.email.to_string();
-        } else {
-            return Err(Box::new(std::io::Error::new(
-                std::io::ErrorKind::InvalidInput,
-                format!("Request failed with status code: {}", response.status()),
-            )));
-        }
+        let text = response.text().await?;
+        let ai: AddressInfo = serde_json::from_str(&text)?;
+
+        self.address_info = Some(ai);
 
         Ok(())
     }
+
+    pub async fn get_mail(&self) -> Result<Vec<Email>, Box<dyn std::error::Error>> {
+        let response = self.client.get(format!("{}index/refresh", DISPOSABLE_MAIL))
+            .headers(headers(true))
+            .send()
+            .await?
+            .error_for_status()?;
+
+        let text = response.text().await?;
+        let mail = Email::list_from_str(text)?;
+        
+        Ok(mail)
+    }
+}
+
+impl Email {
+    fn list_from_str(text: String) -> Result<Vec<Self>, Box<dyn std::error::Error>> {
+        let mut mail: Vec<Email> = serde_json::from_str(&text)?;
+        for m in &mut mail {
+            m.read = m.read_str.to_lowercase() != "new";
+        }
+        Ok(mail)
+    }
+}
+
+pub struct Inbox {
+    client: Client,
+    pub address_info: Option<AddressInfo>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AddressInfo {
+    pub email: String,
+
+    #[serde(rename = "heslo")]
+    _password: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Email {
+    pub id: i32,
+
+    #[serde(rename = "predmet")]
+    pub subject: String,
+
+    #[serde(rename = "od")]
+    pub from: String,
+
+    #[serde(rename = "kdy")]
+    pub when: String,
+
+    #[serde(skip)]
+    pub read: bool,
+
+    #[serde(rename = "precteno")]
+    read_str: String,
 }
 
 pub fn headers(xml_http: bool) -> header::HeaderMap {
@@ -56,12 +102,4 @@ pub fn headers(xml_http: bool) -> header::HeaderMap {
         headers.insert("X-Requested-With", header::HeaderValue::from_static("XMLHttpRequest"));
     }
     headers
-}
-
-#[derive(Debug, Deserialize)]
-struct AddressInfo {
-    email: String,
-
-    #[serde(rename = "heslo")]
-    _password: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 mod mail;
 
+use mail::Inbox;
+use tokio::time::{Duration, sleep};
+
 #[tokio::main]
 async fn main() {
 
-    let mut inbox = mail::Inbox::new();
+    let mut inbox = Inbox::new();
 
     match inbox.establish_address().await {
-        Ok(()) => {},
+        Ok(()) => {
+            let ai = inbox.address_info.as_ref().unwrap();
+            println!("email: {}", ai.email);
+        },
         Err(e) => {
             eprintln!("error: {}", e);
         }
     };
 
-    println!("{}", inbox.address);
+    let interval = Duration::from_secs(30);
+    sleep(interval).await;
+
+    loop {
+        match inbox.get_mail().await {
+            Ok(mail) => {
+                println!("{:?}", mail);
+            },
+            Err(e) => {
+                eprintln!("mail error: {}", e);
+            }
+        };
+        sleep(interval).await;
+    }
 
 }
