e8132af98bf84b937e9a66da0e31d3367257302c
Justin <justin@garofolo.net>
2023-10-06 01:33:13 -0400

    use lazy_static for inbox mutex, populate all emails

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 mod mail;
 
+extern crate lazy_static;
+
 use mail::{Inbox, Email};
 use tokio::time::{Duration, sleep};
+use std::sync::{Mutex, MutexGuard};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref INBOX: Mutex<Inbox> = Mutex::new(Inbox::new());
+}
 
 #[tokio::main]
 async fn main() {
 
-    let mut inbox = Inbox::new();
+    let mut inbox = INBOX.lock().unwrap();
 
     match inbox.establish_address().await {
         Ok(()) => {
@@ -27,14 +35,21 @@ async fn main() {
 
     println!("emails: {:?}", emails);
 
-    let email = emails.get_mut(0).unwrap();
+    populate_emails(&mut inbox, &mut emails).await;
     
-    if let Err(err) = inbox.populate_content(email).await {
-        eprintln!("failed to populate email content: {}", err);
-    }
-    
-    println!("emails: {:?}", emails);
+    println!("emails [populated]: {:?}", emails);
 
     inbox.save_cookies();
 
+}
+
+async fn populate_emails<'a>(
+    inbox: &'a MutexGuard<'a, Inbox>, 
+    emails: &mut Vec<Email>
+) {
+    for email in emails {
+        if let Err(err) = inbox.populate_content(email).await {
+            eprintln!("failed to populate email [ID: {}] content: {}", email.id, err);
+        }
+    }
 }
